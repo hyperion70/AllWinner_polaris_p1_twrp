889995b3c4b4a996eb810a81994c3bdc584544c2
hyperion70 <bmaximov71@gmail.com>
2015-10-16 23:56:24 +0300

    ADD CFLAGS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,10 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_BOOTLOADER_BOARD_NAME := polaris_p1
 
+# CFLAGS
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a7 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a7 -mfpu=neon -mfloat-abi=softfp
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyS0,115200 rw init=/init loglevel=5 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x40000000
@@ -45,13 +49,10 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 1055916032
 # BOARD_CUSTOM_RECOVERY_KEYMAPPING := $(LOCAL_PATH)/recovery_keys.c
 
 # TWRP recovery
-BOARD_HAS_SDCARD_INTERNAL := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGB565"
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
 DEVICE_RESOLUTION := 800x480
-TW_FLASH_FROM_STORAGE := true
 TW_NO_REBOOT_BOOTLOADER := true
-TW_HAS_DUAL_STORAGE := true
-TWRP_EVENT_LOGGING := true
+TW_NO_REBOOT_RECOVERY := true
 TW_INTERNAL_STORAGE_PATH := "/emmc"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "emmc"
 TW_EXTERNAL_STORAGE_PATH := "/sdcard"
@@ -61,6 +62,6 @@ BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
 BOARD_UMS_2ND_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun1/file"
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/twrp.fstab
 BOARD_HAS_NO_SELECT_BUTTON := true
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_MAX_BRIGHTNESS := 255
+# TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+# TW_MAX_BRIGHTNESS := 255
 
